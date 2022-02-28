b368fff1bd68e545a9f43fea76a75bf43a86700a
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-02-28 18:31:59 +0200

    feat: add draw_circle method

--- src/screen.py
+++ src/screen.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import pyautogui
 import cv2
 import numpy as np
@@ -16,6 +17,23 @@ class Screen:
         screenshot = pyautogui.screenshot()
         frame = np.array(screenshot)
         return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    def draw_cirle(self, frame: np.ndarray, center: Tuple[int, int], radius: int) -> np.ndarray:
+        """
+            Draws circle on given image.
+
+            Parameters:
+                frame (np.ndarray): image
+                center (Tuple[int, int]): coordinates of circle
+                radius (int): circle radius
+
+            Returns:
+                image (np.ndarray)
+        """
+        return cv2.circle(frame, center, radius, (0, 0, 255), 3)
+
+    def save_frame(self, frame: np.ndarray,) -> None:
+        cv2.imwrite(f"detected{np.sum(frame)}.png", frame)
 
 
 if __name__ == "__main__":
