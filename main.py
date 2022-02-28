642eda7a0ad827e92766d103e282227889b8c68b
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-02-28 18:32:22 +0200

    feat: main loop modification

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+import cv2
 from src.screen import Screen
 from src.motion_detector import MotionDetector
 from src.alarm import Alarm
+from datetime import datetime
 
 import time
 
 if __name__ == "__main__":
     alarm = Alarm("src\\alarms\\Alarm-ringtone.mp3")
-    motion_detector = MotionDetector()
+    motion_detector = MotionDetector(contour_threshold=10**3)
     screen = Screen()
     previous_frame = motion_detector.process_frame(screen.take_screenshot())
     while True:
-        current_frame = motion_detector.process_frame(screen.take_screenshot())
-        has_mootion = motion_detector.has_movement(previous_frame, current_frame)
-        if has_mootion:
-            alarm.turn_on(2)
-        previous_frame = current_frame
-        time.sleep(1)
+        current_frame = screen.take_screenshot()
+        processed_current_frame = motion_detector.process_frame(current_frame)
+        if motion_detector.has_movement(previous_frame, processed_current_frame):
+            alarm.turn_on(1)
+            moving_objects = motion_detector.detect_movement(previous_frame, processed_current_frame)
+
+            # highlighted_frame = screen.draw_cirle(current_frame, (100, 100), 10)
+            # screen.save_frame(highlighted_frame)
+        previous_frame = processed_current_frame
+        # time.sleep(1)
