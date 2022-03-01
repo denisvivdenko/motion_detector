931ced42234e8358bbc54c864faf0f2dd8493a7c
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-03-02 01:21:13 +0200

    feat: tuning model parameters

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
-import cv2
-from src.object_highlighter import ObjectHighlighter
-from src.screen import Screen
-from src.motion_detector import MotionDetector
-from src.alarm import Alarm
-from datetime import datetime
-import numpy as np
+import json
 
 from src.video_motion_detector import VideoMotionDetector
 
 if __name__ == "__main__":
     input_video = "src\\videos\\camera_1.mp4"
-    parameters = {
-        "guassian_blur_parameters": (3, 3), 
-        "threshold": 30,
-        "erosion_kernel_shape": (1, 1), 
-        "dilation_kernel_shape": (1, 1),
-        "erosion_iterations": 3,
-        "dilation_iterations": 3
-    }
+    with open("src\\parameters.json") as json_file:
+        parameters = json.load(json_file)
 
-    video_motion_detector = VideoMotionDetector(input_video, parameters)
-    video_motion_detector.save_output("src\\videos\\outputs\\test_1.avi")
+    for model_parameters in parameters:    
+        output_name = f"src\\videos\\outputs\\{str(model_parameters.items())}.avi"
+        video_motion_detector = VideoMotionDetector(input_video, model_parameters)
+        video_motion_detector.save_output(output_name)
     
