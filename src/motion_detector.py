440223e7f8dc4507eea1e7afeea8683ee5d3ea29
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-03-02 00:36:31 +0200

    feat: add motion detector parameters to constructor

--- src/motion_detector.py
+++ src/motion_detector.py
@@ -1,18 +1,18 @@
 from collections import namedtuple
-import time
-from datetime import date, datetime
 from typing import List, Tuple
 
 import cv2
 import numpy as np
-import pandas as pd
 
 DetectedObject = namedtuple("DetectedObject", ["x", "y", "height", "width"])
 
 class MotionDetector:
     def __init__(self, guassian_blur_parameters: Tuple[int, int] = (1, 1),
+                    threshold: int = 30,
                     erosion_kernel_shape: Tuple[int, int] = (1, 1),
-                    dilation_kernel_shape: Tuple[int, int] = (1, 1)
+                    dilation_kernel_shape: Tuple[int, int] = (1, 1),
+                    erosion_iterations: int = 1,
+                    dilation_iterations: int = 1
         ) -> None:
         """
             Detects motion between frames.
@@ -24,8 +24,11 @@ class MotionDetector:
                 contour_threshold (int): countour threshold length
         """
         self.guassian_blur_parameters = guassian_blur_parameters
+        self.threshold = threshold
         self.erosion_kernel_shape = erosion_kernel_shape
         self.dilation_kernel_shape = dilation_kernel_shape
+        self.erosion_iterations = erosion_iterations
+        self.dilation_iterations = dilation_iterations
 
     def has_movement(self, difference_frame: np.ndarray, area_threshold: int) -> bool:
         """
@@ -39,7 +42,8 @@ class MotionDetector:
         """
         return np.sum(difference_frame > 0) > area_threshold
 
-    def detect_movement(self, previous_frame: np.ndarray, current_frame: np.ndarray) -> List[DetectedObject]: # TODO
+    # TODO
+    def detect_movement(self, previous_frame: np.ndarray, current_frame: np.ndarray) -> List[DetectedObject]: 
         """
             Detects moving objects coordinates.
 
@@ -84,9 +88,8 @@ class MotionDetector:
         erosion_kernel = np.ones(self.erosion_kernel_shape, np.uint8)
         dilation_kernel = np.ones(self.dilation_kernel_shape, np.uint8)
         processed_frame = cv2.threshold(frame, threshold, 255, cv2.THRESH_BINARY)[1]
-        # processed_frame = cv2.dilate(processed_frame, dilation_kernel, iterations=1)
-        processed_frame = cv2.erode(processed_frame, erosion_kernel, iterations=2)
-        processed_frame = cv2.dilate(processed_frame, dilation_kernel, iterations=1)
+        processed_frame = cv2.erode(processed_frame, erosion_kernel, iterations=self.erosion_iterations)
+        processed_frame = cv2.dilate(processed_frame, dilation_kernel, iterations=self.dilation_iterations)
         return processed_frame
 
     def compute_difference_frame(self, frame_1: np.ndarray, frame_2: np.ndarray) -> np.ndarray:
@@ -94,6 +97,6 @@ class MotionDetector:
             Computes difference between two frames and removes noise.
         """
         difference_frame = cv2.absdiff(frame_1, frame_2)
-        difference_frame = self.remove_noise(difference_frame)
+        difference_frame = self.remove_noise(difference_frame, threshold=self.threshold)
         return difference_frame
 
