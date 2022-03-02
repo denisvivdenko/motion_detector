4dee165fc07e0c3d3ac55536c51ff16c53e31441
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-03-02 10:49:49 +0200

    feat: add parameters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-
+from os import path
 from src.video_motion_detector import VideoMotionDetector
 
 if __name__ == "__main__":
@@ -9,6 +9,9 @@ if __name__ == "__main__":
 
     for model_parameters in parameters:    
         output_name = f"src\\videos\\outputs\\{str(model_parameters.items())}.avi"
+        if path.isfile(output_name):
+            continue
+
         video_motion_detector = VideoMotionDetector(input_video, model_parameters)
         video_motion_detector.save_output(output_name)
     
